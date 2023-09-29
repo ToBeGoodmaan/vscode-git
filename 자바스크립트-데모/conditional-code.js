830f91cea28086da54a8151514b29b03f62dff59
Kim minji <emart1008@naver.com>
2023-09-29 18:27:20 +0900

    v2

---
+++ 자바스크립트-데모/conditional-code.js
@@ -0,0 +1,17 @@
+const myName = 'MinJi';
+
+if (myName === 'MinJi') {
+    console.log('Helo!');
+}
+
+let isLoggedIn = true;
+
+if (!isLoggedIn) {
+    console.log('User is  NOT logged in!');
+}
+
+const enteredUserName = ' '; // 0 빈문자열이나 0은 거짓
+
+if (enteredUserName) {
+    console.log('Input is valid!');
+}
--- 자바스크립트-데모/demo.js
+++ 자바스크립트-데모/demo.js
@@ -12,6 +12,21 @@ function updateRemainingCharacters(event) {
     const remainingCharacters = maxAllowedChars - enteredTextLength;
 
     remainingCharsElement.textContent = remainingCharacters;
+
+    if (remainingCharacters === 0) //remainingCharacters라는 상수가 0자여서 true '참'일 경우 / 상수 값이 0보다 커서 false '거짓'일 경우 
+    //else if 조건문을 검증.
+    {
+        remainingCharsElement.classList.add('error'); //'warning'이라는 클라스의 css 스타일을 작동하도록 시켜라
+        productNameInputElement.classList.add('error'); // 'warning'이라는 클라스의 css스타일을 작동하도록 시켜라.
+    } else if (remainingCharacters <= 10) {
+        remainingCharsElement.classList.add('warning'); 
+        productNameInputElement.classList.add('warning');
+        remainingCharsElement.classList.remove('error');
+        productNameInputElement.classList.remove('error');
+    } else {
+        remainingCharsElement.classList.remove('warning');
+        productNameInputElement.classList.remove('warning');
+    }
 }
 
 productNameInputElement.addEventListener('input',updateRemainingCharacters );
